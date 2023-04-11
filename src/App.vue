860fdc9600e7177575d0dc497dcdc93388f3233b
Sreesanth46 <sreesanth46@gmail.com>
2023-04-11 09:36:20 +0530

    feat: Navbar component, authUser state

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
+    <TheNavbar />
     <div class="container">
         <router-view />
     </div>
 </template>
+
+<script>
+import TheNavbar from '@/components/TheNavbar.vue'
+export default {
+    components: {
+        TheNavbar
+    }
+}
+</script>
