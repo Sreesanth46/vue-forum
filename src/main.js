2fd37fb57a59d962e339ec326487aae9faf7927c
Sreesanth46 <sreesanth46@gmail.com>
2023-04-11 09:36:20 +0530

    fix: global component auto loading - not working

--- src/main.js
+++ src/main.js
@@ -11,6 +11,19 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+// TODO: find a way to handle global components
+
+// const requireComponent = require.context("./components", true, /App[A-Z]\w+\.(vue)$/)
+// requireComponent.key().forEach((filename) => {
+//     let baseComponentConfig = requireComponent(filename)
+//     baseComponentConfig = baseComponentConfig.default || baseComponentConfig
+//     const baseComponentName = baseComponentConfig.name || (
+//         filename.replace(/^.+\//, '').replace(/\.\w+$/, '')
+//     )
+//     app.component(baseComponentName, baseComponentConfig)
+// });
+
 app.component('AppDate', AppDate)
 
 app.mount('#app')
