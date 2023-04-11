b41032efaf28717037dc0b5eb25a79784b62a1bd
Sreesanth46 <sreesanth46@gmail.com>
2023-04-11 09:36:20 +0530

    feat: threadlist component, thread by id - route

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import PageHome from '@/components/PageHome.vue'
+import PageThreadShow from '@/components/PageThreadShow.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: PageHome
-  }
+	{
+		path: '/',
+		name: 'Home',
+		component: PageHome
+	},
+
+	{
+		path: '/thread/:id',
+		name: 'ThreadShow',
+		component: PageThreadShow,
+		props: true,
+	},
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes
 })
 
 export default router
