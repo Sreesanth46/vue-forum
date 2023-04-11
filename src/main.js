c4e81e5c2c93a6397ec0511f6c8599d3aba88c0d
Sreesanth46 <sreesanth46@gmail.com>
2023-04-11 09:36:20 +0530

    feat: vuex store

--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+// import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 import AppDate from '@/components/AppDate.vue'
+import store from '@/stores'
 
 import './assets/style.css'
 
 const app = createApp(App)
 
-app.use(createPinia())
+// app.use(createPinia())
 app.use(router)
+app.use(store)
 
 // TODO: find a way to handle global components
 
