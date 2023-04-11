4758208b04e9a4c18147b7a7deb066a4f2cf1aad
Sreesanth46 <sreesanth46@gmail.com>
2023-04-11 09:36:20 +0530

    feat: redirect-not found if threadshow id invalid

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import PageHome from '@/components/PageHome.vue'
 import PageThreadShow from '@/components/PageThreadShow.vue'
 import PageNotFound from '@/components/PageNotFound.vue'
+import sourceData from '@/data.json'
 
 const routes = [
 	{
@@ -15,10 +16,25 @@ const routes = [
 		name: 'ThreadShow',
 		component: PageThreadShow,
 		props: true,
+		beforeEnter(to, from, next) {
+			const threadExist = sourceData.threads.find(thread => thread.id === to.params.id);
+
+			if (threadExist) {
+				return next();
+			} else {
+				next({
+					name: 'NotFound',
+					params: { pathMatch: to.path.substring(1).split('/') }, // substring removes extra slash(/) in the url
+					// keep query and hash
+					query: to.query,
+					hash: to.hash
+				})
+			}
+		}
 	},
 
 	{
-		path: '/:pathMatch(.*)',
+		path: '/:pathMatch(.*)*',
 		name: 'NotFound',
 		component: PageNotFound,
 		props: true,
