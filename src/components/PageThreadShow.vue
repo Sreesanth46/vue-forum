a2862ca58261ccc654df3374a4a2d76b36aba298
Sreesanth46 <sreesanth46@gmail.com>
2023-04-11 09:36:20 +0530

    feat: show thread page

---
+++ src/components/PageThreadShow.vue
@@ -0,0 +1,38 @@
+<template>
+    <h1>Welcome to the forum</h1>
+    <ThreadList />
+</template>
+
+<script>
+import sourceData from '@/data.json'
+
+export default {
+    props: {
+        id: { required: true, type: String }
+    },
+
+    data() {
+        return {
+            threads: sourceData.threads,
+            posts: sourceData.posts,
+            users: sourceData.users
+        }
+    },
+
+    computed: {
+        thread() {
+            return this.threads.find((thread) => thread.id === this.id)
+        }
+    },
+
+    methods: {
+        postById(postId) {
+            return this.posts.find((p) => p.id === postId)
+        },
+
+        userById(userId) {
+            return this.users.find((p) => p.id === userId)
+        }
+    }
+}
+</script>
