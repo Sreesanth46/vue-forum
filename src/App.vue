76a59add128059971ffaeba96d8b45aa62373e86
Sreesanth46 <sreesanth46@gmail.com>
2023-04-11 09:36:20 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import PageHome from '@/components/PageHome.vue'
+
+export default {
+    components: {
+        PageHome
+    }
+}
+</script>
+
+<template>
+    <PageHome></PageHome>
+    <RouterView />
+</template>
